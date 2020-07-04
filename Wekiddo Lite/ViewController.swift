9fbfae5d839d29dac50711548a9753ab65644b55
santmalvin17 <malvinsantoso023@gmail.com>
2020-07-04 16:43:31 +0700

    tesst smua

---
+++ Wekiddo Lite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wekiddo Lite
+//
+//  Created by Malvin Santoso on 04/07/20.
+//  Copyright © 2020 Malvin Santoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
